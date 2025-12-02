01016b10df91bfa24bf99b578e17e74cbcd0d566
Elliot Yates <elliotyatesj@gmail.com>
2025-12-02 14:24:11 +0000

    Working on AOC 2023 day 1 part 2

--- 2023/day_01/day_01.py
+++ 2023/day_01/day_01.py
@@ -4,15 +4,20 @@ part2 = 0
 # Parse the input file, line by line
 input = [line.strip() for line in open("day_01_input.txt", 'r')]
 
+# Numbers as text
+text_nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+
 for line in input:
     # Extract the numbers in the line, keeping as a string for easy merging
-    all_numbers = [n for n in list(line) if n.isdigit()]
+    all_digits = [n for n in list(line) if n.isdigit()]
 
-    # Merge the first and last number to obtain the line's calibration value
-    calibration_value = all_numbers[0] + all_numbers[-1]
+    # Extract the numbers in the line, from text form
+    all_text_nums = [num for num in text_nums if num in line]
+    # TODO: But they need to be matched together, respecting order
 
-    # Part1 - sum of calibration values
-    part1 += int(calibration_value)
+    # Part1 - sum of digit calibration values
+    digit_calibration_value = all_digits[0] + all_digits[-1]
+    part1 += int(digit_calibration_value)
 
 print("Part 1:", part1)
 print("Part 2:", part2)
