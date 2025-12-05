80bcb0a438af54de0c5da52174e39c969b42bcb9
Elliot Yates <elliotyatesj@gmail.com>
2025-12-05 22:35:35 +0000

    AOC 2015 day_02 parts 1 and 2 solved

---
+++ 2015/day_02/day_02.py
@@ -0,0 +1,22 @@
+part1 = 0
+part2 = 0
+
+# Parse the input file, line by line
+input = [line.strip() for line in open("day_02_input.txt", 'r')]
+
+for line in input:
+    # Extract L, W, H from input
+    l, w, h = map(int, line.split("x"))
+
+    # Part1 - sum required paper plus additional for smallest side
+    s1, s2, s3 = 2*l*w, 2*w*h, 2*h*l
+    part1 += sum([s1, s2, s3, min(l*w, w*h, h*l)])
+
+    # Part2 - sum ribbon length
+    sides = sorted([l, w, h])
+    # Sort by smallest sides
+    rib_len = 2*sides[0] + 2*sides[1] + (l*w*h)
+    part2 += rib_len
+
+print("Part 1:", part1)
+print("Part 2:", part2)
